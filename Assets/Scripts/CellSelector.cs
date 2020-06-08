67f98e5570252416edd3e4dd097d9986a8c1bdd5
Jake Card <jacob.boxer@gmail.com>
2020-06-08 13:38:41 -0700

    Add CellSelector

---
+++ Assets/Scripts/CellSelector.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellSelector : MonoBehaviour {
+    private Camera mainCamera;
+
+    private void Start() {
+        mainCamera = Camera.main;
+    }
+
+    private void Update() {
+        RaycastHit hit;
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+    }
+}
