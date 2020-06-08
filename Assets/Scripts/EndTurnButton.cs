d3c67f664faccee060b5b4a460add7875ea5f146
Jessica Card <jessicard@mac.com>
2020-06-08 14:41:07 -0700

    Formatting

--- Assets/Scripts/EndTurnButton.cs
+++ Assets/Scripts/EndTurnButton.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EndTurnButton : MonoBehaviour
-{
+public class EndTurnButton : MonoBehaviour {
     public void EndTurn() {
         Turn.Instance.EndTurn();
     }
