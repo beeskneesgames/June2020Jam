ca50e6908f0333340cc15cb3fab6cc27d54be678
Jessica Card <jessicard@mac.com>
2020-06-08 16:42:40 -0700

    Get pausing working

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+    public GameObject menuContainer;
+
+    private float oldTimeScale;
+
+    private void Start() {
+        oldTimeScale = Time.timeScale;
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (Globals.isPaused) {
+                Resume();
+            } else {
+                Pause();
+            }
+        }
+    }
+
+    private void OnDestroy() {
+        Resume();
+    }
+
+    public void Resume() {
+        Globals.isPaused = false;
+        Time.timeScale = oldTimeScale;
+
+        menuContainer.SetActive(false);
+    }
+
+    public void Pause() {
+        Globals.isPaused = true;
+        oldTimeScale = Time.timeScale;
+        Time.timeScale = 0.0f;
+
+        menuContainer.SetActive(true);
+    }
+
+    public void ExitGame() {
+        SceneLoader.StartTitleScene();
+    }
+}
