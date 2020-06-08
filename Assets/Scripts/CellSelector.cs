fe84ea80abad67276d26bd3f68b5ec8f7299147a
Jake Card <jacob.boxer@gmail.com>
2020-06-08 15:30:55 -0700

    Show action canvas on click

--- Assets/Scripts/CellSelector.cs
+++ Assets/Scripts/CellSelector.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class CellSelector : MonoBehaviour {
     public TextMeshProUGUI coordsText;
     public Grid grid;
+    public Canvas actionCanvas;
     private Camera mainCamera;
     private int cellLayerMask;
 
@@ -18,6 +20,7 @@ public class CellSelector : MonoBehaviour {
         RaycastHit hit;
         Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
         Cell hitCell = null;
+        bool clicked = Input.GetMouseButtonDown(0);
 
         if (Physics.Raycast(ray, out hit, Mathf.Infinity, cellLayerMask)) {
             hitCell = hit.transform.GetComponent<Cell>();
@@ -26,9 +29,24 @@ public class CellSelector : MonoBehaviour {
         if (hitCell) {
             coordsText.text = $"({hitCell.Coords.x}, {hitCell.Coords.y})";
             grid.SetHoveredCoords(hitCell.Coords);
+
+            if (clicked) {
+                actionCanvas.transform.position = new Vector3(
+                    hitCell.transform.position.x + 2.0f,
+                    hitCell.transform.position.y + 4.0f,
+                    hitCell.transform.position.z + 3.5f
+                );
+                // TODO do this nicer
+                actionCanvas.transform.GetChild(0).gameObject.SetActive(true);
+            }
         } else {
             coordsText.text = "(?, ?)";
             grid.ClearHoveredCoords();
+
+            if (clicked) {
+                // TODO do this nicer
+                actionCanvas.transform.GetChild(0).gameObject.SetActive(false);
+            }
         }
 
     }
