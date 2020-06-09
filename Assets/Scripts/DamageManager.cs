51642a11c805557c541fe1432b9feb631d7ba3a5
Jessica Card <jessicard@mac.com>
2020-06-08 18:02:33 -0700

    Create DamageManager

---
+++ Assets/Scripts/DamageManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageManager : MonoBehaviour {
+    private static DamageManager instance;
+    private List<Vector2Int> damageHeads;
+
+    public static DamageManager Instance {
+        get {
+            return instance;
+        }
+    }
+
+    private void Awake() {
+        if (instance != null && instance != this) {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+    }
+
+    private void Start() {
+        AddHead();
+    }
+
+    private void AddHead() {
+        Vector2Int damageHead = new Vector2Int(-1, -1);
+
+        damageHeads.Add(damageHead);
+    }
+}
