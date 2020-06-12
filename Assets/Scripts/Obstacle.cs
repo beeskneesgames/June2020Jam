a55438a899f3bba4df3a6af15b58eb628ac0e6b7
Jessica Card <jessicard@mac.com>
2020-06-11 17:03:28 -0700

    Add loops

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour {
     public Vector2Int Coords { get; set; } = new Vector2Int(-1, -1);
-    private int count = 3;
+    private int smallCount = 2;
+    private int bigCount = 1;
 
     private void Start() {
         Generate();
     }
 
     private void Generate() {
+        // Generate small obstacles
+        for (int count = 0; count < smallCount; count++) {
+        }
 
+        // Generate big obstacles
+        for (int count = 0; count < bigCount; count++) {
+        }
     }
 }
