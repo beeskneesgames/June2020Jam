481742f0488d548b2edc201cc90cd99fe2f28268
Jessica Card <jessicard@mac.com>
2020-06-12 13:42:50 -0700

    Disable moving thru obstacles

--- Assets/Scripts/ActionMenu.cs
+++ Assets/Scripts/ActionMenu.cs
@@ -38,14 +38,20 @@ public class ActionMenu : MonoBehaviour {
 
         if (Grid.Instance.HasSelectedCoords) {
             List<Vector2Int> path = Grid.PathBetween(Player.Instance.CurrentCell.Coords, Grid.Instance.SelectedCoords, Player.diagonalMoveAllowed);
+            bool interactable = true;
 
-            if (Player.Instance.ActionPoints >= path.Count - 1) {
-                moveBtn.interactable = true;
-                fixBtn.interactable = true;
-            } else {
-                moveBtn.interactable = false;
-                fixBtn.interactable = false;
+            foreach (var coords in path) {
+                if (Grid.Instance.CellInfoAt(coords).HasObstacle) {
+                    interactable = false;
+                }
             }
+
+            if (Player.Instance.ActionPoints < path.Count - 1) {
+                interactable = false;
+            }
+
+            moveBtn.interactable = interactable;
+            fixBtn.interactable = interactable;
 
             // -1 because path includes the player's square.
             costText.text = $"Cost: {path.Count - 1} AP";
