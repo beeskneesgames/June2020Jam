cb7e57f4b0bf09b026116855f21abd1d45f1e48e
Jake Card <jacob.boxer@gmail.com>
2020-06-12 13:05:56 -0700

    Prevent overlaps on big obstacle

--- Assets/Scripts/ObstacleManager.cs
+++ Assets/Scripts/ObstacleManager.cs
@@ -3,8 +3,8 @@ using UnityEngine;
 
 public class ObstacleManager : MonoBehaviour {
     public Vector2Int Coords { get; set; } = new Vector2Int(-1, -1);
-    private int smallCount = 2;
-    private int bigCount = 1;
+    private readonly int smallCount = 2;
+    private readonly int bigCount = 1;
 
     private void Start() {
         Generate();
@@ -20,43 +20,42 @@ public class ObstacleManager : MonoBehaviour {
 
         // Generate big obstacles
         for (int count = 0; count < bigCount; count++) {
-            CellInfo cell1 = Grid.Instance.RetrieveRandomCell(2);
-            CellInfo[] bigObstacleCells = Grid.Instance.AdjacentTo(cell1.Coords, false);
+            CellInfo[] bigObstacleCells = new CellInfo[2];
 
-            while (bigObstacleCells.Length < 2) {
-                cell1 = Grid.Instance.RetrieveRandomCell(2);
-                bigObstacleCells = Grid.Instance.AdjacentTo(cell1.Coords, false);
+            do {
+                bigObstacleCells[0] = Grid.Instance.RetrieveRandomCell();
+
+                CellInfo[] adjacentCells = Grid.Instance.AdjacentTo(bigObstacleCells[0].Coords, false);
+                bigObstacleCells[1] = adjacentCells[Random.Range(0, bigObstacleCells.Length)];
+            } while (!IsValidForObstacles(bigObstacleCells));
+
+            foreach (var bigObstacleCell in bigObstacleCells) {
+                bigObstacleCell.AddObstacle();
             }
-
-            CellInfo cell2 = bigObstacleCells[0];
-
-            cell1.AddObstacle();
-            cell2.AddObstacle();
         }
 
         // Generate tower
         CellInfo[] towerCells = new CellInfo[4];
-        bool overlap;
 
         do {
             towerCells[0] = Grid.Instance.RetrieveRandomCell(1);
             towerCells[1] = Grid.Instance.CellInfoAt(towerCells[0].Coords + new Vector2Int(1, 0));
             towerCells[2] = Grid.Instance.CellInfoAt(towerCells[0].Coords + new Vector2Int(0, 1));
             towerCells[3] = Grid.Instance.CellInfoAt(towerCells[0].Coords + new Vector2Int(1, 1));
-
-            overlap = false;
-
-            // Make sure the tower doesn't overlap any of the other obstacles.
-            foreach (var towerCell in towerCells) {
-                if (towerCell.HasObstacle) {
-                    overlap = true;
-                    break;
-                }
-            }
-        } while (overlap);
+        } while (!IsValidForObstacles(towerCells));
 
         foreach (var towerCell in towerCells) {
             towerCell.AddObstacle();
         }
+    }
+
+    private static bool IsValidForObstacles(IEnumerable<CellInfo> cells) {
+        foreach (var cell in cells) {
+            if (cell.HasObstacle || cell.HasDamageHead || cell == Player.Instance.CurrentCell) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
