120909428c8aa09262fffef2523d1f353c0bffd0
Jake Card <jacob.boxer@gmail.com>
2020-06-13 13:36:34 -0700

    Increase max turn count

--- Assets/Scripts/Turn.cs
+++ Assets/Scripts/Turn.cs
@@ -5,7 +5,7 @@ public class Turn : MonoBehaviour {
     private static Turn instance;
     private int turnCount;
 
-    public const int MaxTurnCount = 20;
+    public const int MaxTurnCount = 25;
     public TextMeshProUGUI turnUI;
 
     public int TurnCount {
