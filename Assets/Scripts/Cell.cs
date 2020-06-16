a99b793ca393bcc006440479139da0620d280057
Jake Card <jacob.boxer@gmail.com>
2020-06-16 15:33:30 -0700

    Set up for cell material swapping
    * Add shader for damaged cells
    * Add material database
    * Swap materials in Cell.UpdateAppearance

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -14,6 +14,20 @@ public class Cell : MonoBehaviour {
     private new Renderer renderer;
     private Color originalColor;
 
+    private Material currentMaterial;
+    private Material CurrentMaterial {
+        get {
+            return currentMaterial;
+        }
+
+        set {
+            if (value != currentMaterial) {
+                currentMaterial = value;
+                renderer.material = currentMaterial;
+            }
+        }
+    }
+
     private void Start() {
         renderer = GetComponent<Renderer>();
         originalColor = renderer.material.GetColor("_BaseColor");
@@ -28,28 +42,35 @@ public class Cell : MonoBehaviour {
             case MouseState.None:
                 if (inPath) {
                     // Light gray
+                    CurrentMaterial = MaterialDatabase.Instance.cellHealthy;
                     renderer.material.SetColor("_BaseColor", new Color(0.75f, 0.75f, 0.75f));
                 } else if (Info.HasBomb) {
                     // Black
+                    CurrentMaterial = MaterialDatabase.Instance.cellHealthy;
                     renderer.material.SetColor("_BaseColor", Color.black);
                 } else if (Info.HasObstacle) {
                     // Pink
+                    CurrentMaterial = MaterialDatabase.Instance.cellHealthy;
                     renderer.material.SetColor("_BaseColor", new Color(1.0f, 0.5f, 1.00f));
                 } else if (Info.HasDamageHead) {
                     // Dark red
+                    CurrentMaterial = MaterialDatabase.Instance.cellHealthy;
                     renderer.material.SetColor("_BaseColor", new Color(1.0f, 0.0f, 0.0f));
                 } else if (Info.IsDamaged) {
-                    // Light red
-                    renderer.material.SetColor("_BaseColor", new Color(1.0f, 0.5f, 0.5f));
+                    // Glitch
+                    CurrentMaterial = MaterialDatabase.Instance.cellDamaged;
                 } else {
                     // White
+                    CurrentMaterial = MaterialDatabase.Instance.cellHealthy;
                     renderer.material.SetColor("_BaseColor", originalColor);
                 }
                 break;
             case MouseState.Hovered:
+                CurrentMaterial = MaterialDatabase.Instance.cellHealthy;
                 renderer.material.SetColor("_BaseColor", new Color(1.0f, 1.0f, 0.5f));
                 break;
             case MouseState.Selected:
+                CurrentMaterial = MaterialDatabase.Instance.cellHealthy;
                 renderer.material.SetColor("_BaseColor", new Color(0.5f, 1.0f, 0.5f));
                 break;
         }
