ee3be018fca1b658022b0ef9456cfcc71494bc3f
Jake Card <jacob.boxer@gmail.com>
2020-06-19 21:28:19 -0700

    Open/close instructions

---
+++ Assets/Scripts/InstructionsController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InstructionsController : MonoBehaviour {
+    public Button openButton;
+    public Button closeButton;
+    public GameObject instructionsContainer;
+
+    public void OnOpenButtonClicked() {
+        instructionsContainer.SetActive(true);
+    }
+
+    public void OnCloseButtonClicked() {
+        instructionsContainer.SetActive(false);
+    }
+}
