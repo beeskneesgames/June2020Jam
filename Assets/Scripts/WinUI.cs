f6a3b4015a1f1e6b020814288b8483cd35628d4b
Jessica Card <jessicard@mac.com>
2020-06-21 16:24:33 -0700

    Add win scene

---
+++ Assets/Scripts/WinUI.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class WinUI : MonoBehaviour {
+
+    public void Start() {
+        AudioManager.Instance.Stop("Theme");
+        AudioManager.Instance.Play("Win");
+    }
+
+    public void ResetBoard() {
+        AudioManager.Instance.Stop("Win");
+
+        SceneLoader.StartMainScene();
+    }
+}
