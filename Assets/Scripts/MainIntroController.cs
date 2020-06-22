50e03003259aa9cf3f52a9c8c1c90b2ac6f3cc90
Jessica Card <jessicard@mac.com>
2020-06-21 19:26:22 -0700

    Add to main anim

--- Assets/Scripts/MainIntroController.cs
+++ Assets/Scripts/MainIntroController.cs
@@ -7,17 +7,24 @@ public class MainIntroController : MonoBehaviour {
 
     private void Start() {
         animator = GetComponent<Animator>();
+        AudioManager.Instance.Play("Drive");
     }
 
     public void OnVehicleStopped() {
+        AudioManager.Instance.Stop("Drive");
+        AudioManager.Instance.Play("Run");
         Player.Instance.StartMoveAnimation();
     }
 
     public void OnPlayerWillStop() {
         Player.Instance.StartSkidAnimation();
+        AudioManager.Instance.Stop("Run");
+        AudioManager.Instance.Play("Turn");
     }
 
     public void OnIntroFinished() {
+        AudioManager.Instance.Stop("Turn");
+
         animator.StopPlayback();
         animator.enabled = false;
 
